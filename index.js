c3d5cfb57daaceeb1bee0f3936ecda7573f04635
Eldho Paulose <eldhopaulose0485@gmail.com>
2022-01-16 21:24:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+const authRoute = require('./routes/auth')
+
+const  dbURI = 'mongodb+srv://eldhopaulose0485:aleenaeldho_025@cluster0.4sjqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+app.use(express.json())
+app.use('/api/auth', authRoute)
+
+
+mongoose.connect(dbURI , {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+
+db.on("error", (err)=>{console.error(err)})
+db.once("open", () => {console.log("DB started successfully")})
+
+app.listen(2400, '192.168.0.107', () => {console.log("Server started: 2400")})
+
+
+
+
+
+
